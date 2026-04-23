5ab41e21ea153cb83ddf385df3228d62940cf2b1
Agustin Aurellana <agustinaurellana@sagradocorazon.edu.ar>
2026-04-23 12:48:15 -0300

    Arch: Restructuring to MVC pattern and merging branches

--- v0.0.9/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "nutrimax");
-
-if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
-}
-?>
--- v0.0.9/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "nutrimax");
-// Si falla, que nos avise
-if (!$conexion) { die("No pude conectar: " . mysqli_connect_error()); }
-?>
