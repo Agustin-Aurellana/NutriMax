62c18b41c1b91009268e54c3e3fb55a8b0dd6ba4
Gevaudaan <agustin.aurellana278@gmail.com>
2026-05-22 17:12:28 -0300

    Resuelve conflictos de merge restantes y normalizacion de genero

--- app/Controllers/registro.php
+++ app/Controllers/registro.php
@@ -48,11 +48,7 @@ if ($result['success']) {
             'ID_USER'   => $result['id'],
             'email'     => $data->email,
             'name'      => $data->name ?? '',
-<<<<<<< HEAD
             'genero'    => !empty($data->sex) ? strtoupper(substr($data->sex, 0, 1)) : 'M',
-=======
-            'genero'    => $data->sex ?? '',
->>>>>>> origin/mis-arreglos-santi
             'nacimiento'=> $data->birthDate ?? '',
             'peso'      => $data->weight ?? 0,
             'altura_cm' => $data->height ?? 0,
